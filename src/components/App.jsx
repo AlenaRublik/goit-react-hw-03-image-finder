4631544a340842ccf8212e3fc96db0899603f680
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-05-22 12:25:47 +0300

    Finish SearchBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import SearchBar from './SearchBar/SearchBar';
+
+
+export class App extends Component {
+  state = {
+    searchQuery: '',
+    pictures: [],
+    page: 1,
+    loading: false,
+    loadMore: false,
+    error: '',
+  };
+
+
+handleSearch = searchQuery => {
+    const initialStateParams = {
+      page: 1,
+      pictures: [],
+      error: '',
+      loadMore: false,
+    };
+
+    if (searchQuery === this.state.searchQuery) {
+      toast.error(
+        'The same request. Change your request'
+      );
+      return;
+    }
+
+    this.setState({ ...initialStateParams, searchQuery });
+  };
+
+  render() {
+    const { error, pictures, loading, loadMore } = this.state;
+    return (
+      <>
+        <SearchBar handleSearch={this.handleSearch} />
+      <ToastContainer/>
+      </>
+    );
+  }
+}
